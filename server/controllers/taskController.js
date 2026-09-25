1b4180f53699b364f3cb891360ff3f0d18bd1cf2
Pavan Kumar <pavan.iiit70@gmail.com>
2026-09-25 07:47:28 +0530

    Fix User model import casing

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,6 +1,6 @@
 const Task = require("../models/Task");
 const Project = require("../models/project");
-const User = require("../models/User");
+const User = require("../models/user");
 const Notification = require("../models/Notification");
 const TaskComment = require("../models/TaskComment");
 
