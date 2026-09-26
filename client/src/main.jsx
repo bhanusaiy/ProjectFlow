65a62edcf862fff4eb1d1d4f9c8c18a549e9cdd1
Pavan Kumar <pavan.iiit70@gmail.com>
2026-09-26 08:09:10 +0530

    Connect frontend to deployed backend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-import { AuthProvider } from "./context/authContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 import "./index.css";
 
